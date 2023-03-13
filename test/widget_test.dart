dbf5c883ad1412bbbbabb934fd7c71a941b2c301
MSOB7YY <mohamedsobhy201566@gmail.com>
2023-03-13 23:41:37 +0200

    chore: removed non-android folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:namida/main.dart';
