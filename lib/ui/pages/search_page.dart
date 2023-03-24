9bc80fc8f5bcd5872f200e95bf7362944881f342
MSOB7YY <mohamedsobhy201566@gmail.com>
2023-03-24 04:33:36 +0200

    feat: search page completo

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -1,13 +1,24 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:get/get.dart';
 
 import 'package:namida/controller/indexer_controller.dart';
+import 'package:namida/controller/scroll_search_controller.dart';
+import 'package:namida/controller/settings_controller.dart';
 import 'package:namida/core/constants.dart';
+import 'package:namida/core/extensions.dart';
+import 'package:namida/core/icon_fonts/broken_icons.dart';
 import 'package:namida/core/translations/strings.dart';
+import 'package:namida/main.dart';
+import 'package:namida/ui/pages/albums_page.dart';
+import 'package:namida/ui/pages/artists_page.dart';
 import 'package:namida/ui/widgets/custom_widgets.dart';
 import 'package:namida/ui/widgets/library/album_card.dart';
+import 'package:namida/ui/widgets/library/artist_card.dart';
 import 'package:namida/ui/widgets/library/track_tile.dart';
 
 class SearchPage extends StatelessWidget {
@@ -16,160 +27,180 @@ class SearchPage extends StatelessWidget {
   final ScrollController _scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Container(
-        color: context.theme.scaffoldBackgroundColor,
-        child: Indexer.inst.tracksInfoList.length == Indexer.inst.trackSearchList.length
-            ? Container(
-                width: double.infinity,
-                height: double.infinity,
-                child: const Text("FSAIOMsdiom"),
-              )
-            : AnimationLimiter(
-                child: CustomScrollView(
-                  slivers: [
-                    // Albums
-                    if (Indexer.inst.albumSearchList.isNotEmpty) ...[
-                      SliverToBoxAdapter(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const SizedBox(
-                                width: 12.0,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  Language.inst.ALBUMS,
-                                  style: context.textTheme.displayLarge,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              const FittedBox(
-                                child: CreatePlaylistButton(),
-                              ),
-                              const SizedBox(
-                                width: 8.0,
-                              ),
-                            ],
-                          ),
+    return Container(
+      color: context.theme.scaffoldBackgroundColor,
+      child: Obx(
+        () => AnimatedSwitcher(
+          duration: const Duration(milliseconds: 400),
+          child: Indexer.inst.trackSearchTemp.isEmpty && Indexer.inst.albumSearchTemp.isEmpty && Indexer.inst.artistSearchTemp.isEmpty
+              ? Container(
+                  key: const ValueKey('emptysearch'),
+                  padding: const EdgeInsets.all(64.0).add(const EdgeInsets.only(bottom: 64.0)),
+                  width: context.width,
+                  height: context.height,
+                  child: Opacity(
+                    opacity: 0.8,
+                    child: ImageFiltered(
+                      imageFilter: ScrollSearchController.inst.isGlobalSearchMenuShown.value
+                          ? ImageFilter.blur(
+                              sigmaX: 4.0,
+                              sigmaY: 4.0,
+                            )
+                          : ImageFilter.blur(
+                              sigmaX: 12.0,
+                              sigmaY: 12.0,
+                            ),
+                      child: Image.asset('assets/namida_icon.png'),
+                    ),
+                  ),
+                )
+              : AnimationLimiter(
+                  key: const ValueKey('fullsearch'),
+                  child: CupertinoScrollbar(
+                    controller: _scrollController,
+                    child: CustomScrollView(
+                      controller: _scrollController,
+                      slivers: [
+                        const SliverPadding(
+                          padding: EdgeInsets.only(bottom: 8.0),
                         ),
-                      ),
-                      SliverToBoxAdapter(
-                        child: SizedBox(
-                          height: Get.height / 4.2,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: Indexer.inst.albumSearchList.entries
-                                .map(
-                                  (e) => Container(
-                                    width: Get.width / 3,
-                                    margin: const EdgeInsets.symmetric(horizontal: 3.0),
-                                    child: AlbumCard(
-                                      gridCountOverride: 3,
-                                      album: e.value.toList(),
-                                      staggered: false,
-                                    ),
-                                  ),
-                                )
-                                .toList(),
-                          ),
-                        ),
-                      ),
-                      const SliverPadding(
-                        padding: EdgeInsets.only(top: 18.0),
-                      ),
-                    ],
 
-                    // Artists
-                    // if (Indexer.inst.artistSearchList.isNotEmpty) ...[
-                    //   SliverToBoxAdapter(
-                    //     child: Container(
-                    //       padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    //       child: Row(
-                    //         mainAxisAlignment: MainAxisAlignment.start,
-                    //         children: [
-                    //           const SizedBox(
-                    //             width: 12.0,
-                    //           ),
-                    //           Expanded(
-                    //             child: Text(
-                    //               Language.inst.ARTISTS,
-                    //               style: context.textTheme.displayLarge,
-                    //               maxLines: 1,
-                    //               overflow: TextOverflow.ellipsis,
-                    //             ),
-                    //           ),
-                    //           const FittedBox(
-                    //             child: CreatePlaylistButton(),
-                    //           ),
-                    //           const SizedBox(
-                    //             width: 8.0,
-                    //           ),
-                    //         ],
-                    //       ),
-                    //     ),
-                    //   ),
-                    //   SliverToBoxAdapter(
-                    //     child: SizedBox(
-                    //       height: Get.height / 4.2,
-                    //       child: ListView(
-                    //         scrollDirection: Axis.horizontal,
-                    //         children: Indexer.inst.albumSearchList.entries
-                    //             .map(
-                    //               (e) => Container(
-                    //                 width: Get.width / 3,
-                    //                 margin: const EdgeInsets.symmetric(horizontal: 3.0),
-                    //                 child: AlbumCard(
-                    //                   gridCountOverride: 3,
-                    //                   album: e.value.toList(),
-                    //                   staggered: false,
-                    //                 ),
-                    //               ),
-                    //             )
-                    //             .toList(),
-                    //       ),
-                    //     ),
-                    //   ),
-                    //   const SliverPadding(
-                    //     padding: EdgeInsets.only(top: 18.0),
-                    //   ),
-                    // ],
-                    // Tracks
-                    if (Indexer.inst.trackSearchList.isNotEmpty)
-                      SliverList(
-                        delegate: SliverChildBuilderDelegate(
-                          (context, i) {
-                            final album = Indexer.inst.trackSearchList
-                                .asMap()
-                                .entries
-                                .map(
-                                  (e) => TrackTile(track: e.value),
-                                )
-                                .toList();
-                            return AnimationConfiguration.staggeredList(
-                              position: i,
-                              duration: const Duration(milliseconds: 400),
-                              child: SlideAnimation(
-                                verticalOffset: 25.0,
-                                child: FadeInAnimation(
-                                  duration: const Duration(milliseconds: 400),
-                                  child: album[i],
-                                ),
+                        /// Albums
+                        if (Indexer.inst.albumSearchTemp.isNotEmpty) ...[
+                          SliverToBoxAdapter(
+                            child: SearchPageTitleRow(
+                              title: Language.inst.ALBUMS,
+                              icon: Broken.music_dashboard,
+                              buttonIcon: Broken.category,
+                              buttonText: Language.inst.VIEW_ALL,
+                              onPressed: () {
+                                Get.offAll(() => MainPageWrapper(
+                                      getOffAll: true,
+                                      title: Text(Language.inst.ALBUMS),
+                                      child: AlbumsPage(albums: Indexer.inst.albumSearchTemp),
+                                    ));
+                                ScrollSearchController.inst.isGlobalSearchMenuShown.value = false;
+                              },
+                            ),
+                          ),
+                          const SliverPadding(
+                            padding: EdgeInsets.only(bottom: 12.0),
+                          ),
+                          SliverToBoxAdapter(
+                            child: SizedBox(
+                              height: 170,
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: Indexer.inst.albumSearchTemp
+                                    .map(
+                                      (e) => Container(
+                                        width: 130,
+                                        margin: const EdgeInsets.only(left: 2.0),
+                                        child: AlbumCard(
+                                          gridCountOverride: 3,
+                                          album: e.tracks,
+                                          staggered: false,
+                                        ),
+                                      ),
+                                    )
+                                    .toList(),
                               ),
-                            );
-                          },
-                          childCount: Indexer.inst.trackSearchList.length,
-                        ),
-                      ),
-                    const SliverPadding(
-                      padding: EdgeInsets.only(bottom: kBottomPadding),
-                    )
-                  ],
+                            ),
+                          ),
+                          const SliverPadding(
+                            padding: EdgeInsets.only(top: 12.0),
+                          ),
+                        ],
+
+                        /// Artists
+                        if (Indexer.inst.artistSearchTemp.isNotEmpty) ...[
+                          SliverToBoxAdapter(
+                            child: SearchPageTitleRow(
+                              title: Language.inst.ARTISTS,
+                              icon: Broken.profile_2user,
+                              buttonIcon: Broken.category,
+                              buttonText: Language.inst.VIEW_ALL,
+                              onPressed: () {
+                                Get.offAll(() => MainPageWrapper(
+                                      getOffAll: true,
+                                      title: Text(Language.inst.ARTISTS),
+                                      child: ArtistsPage(artists: Indexer.inst.artistSearchTemp),
+                                    ));
+                                ScrollSearchController.inst.isGlobalSearchMenuShown.value = false;
+                              },
+                            ),
+                          ),
+                          const SliverPadding(
+                            padding: EdgeInsets.only(bottom: 12.0),
+                          ),
+                          SliverToBoxAdapter(
+                            child: SizedBox(
+                              height: 100,
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: Indexer.inst.artistSearchTemp
+                                    .map(
+                                      (e) => Container(
+                                        width: 80,
+                                        margin: const EdgeInsets.only(left: 2.0),
+                                        child: ArtistCard(
+                                          gridCount: 5,
+                                          name: e.name,
+                                          artist: e.tracks,
+                                        ),
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            ),
+                          ),
+                          const SliverPadding(
+                            padding: EdgeInsets.only(top: 12.0),
+                          ),
+                        ],
+
+                        /// Tracks
+                        if (Indexer.inst.trackSearchTemp.isNotEmpty) ...[
+                          SliverToBoxAdapter(
+                              child: Tooltip(
+                            message: Language.inst.TRACK_PLAY_MODE,
+                            child: SearchPageTitleRow(
+                              title: Language.inst.TRACKS,
+                              icon: Broken.music_circle,
+                              buttonIcon: Broken.play,
+                              buttonText: SettingsController.inst.trackPlayMode.value.toText,
+                              onPressed: () => SettingsController.inst.trackPlayMode.value.toggleSetting(),
+                            ),
+                          )),
+                          const SliverPadding(
+                            padding: EdgeInsets.only(bottom: 12.0),
+                          ),
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, i) {
+                                final track = Indexer.inst.trackSearchTemp[i];
+                                return AnimatingTile(
+                                  position: i,
+                                  child: TrackTile(
+                                    index: i,
+                                    track: track,
+                                    queue: SettingsController.inst.trackPlayMode.value.getQueue(track, searchQueue: Indexer.inst.trackSearchTemp.toList()),
+                                  ),
+                                );
+                              },
+                              childCount: Indexer.inst.trackSearchTemp.length,
+                            ),
+                          ),
+                        ],
+
+                        const SliverPadding(
+                          padding: EdgeInsets.only(bottom: kBottomPadding),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+        ),
       ),
     );
   }
