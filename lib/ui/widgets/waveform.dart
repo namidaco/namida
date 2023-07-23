26b1496db6c1dc05a37955927cf2b2635d4991b6
MSOB7YY <mohamedsobhy201566@gmail.com>
2023-07-24 02:09:52 +0300

    perf: prevent reduntant waveform rebuilds

--- lib/ui/widgets/waveform.dart
+++ lib/ui/widgets/waveform.dart
@@ -7,13 +7,15 @@ import 'package:namida/controller/settings_controller.dart';
 import 'package:namida/controller/waveform_controller.dart';
 import 'package:namida/core/extensions.dart';
 
-class WaveformComponent extends StatelessWidget {
+class WaveformComponent extends StatefulWidget {
   final int durationInMilliseconds;
   final Color? color;
   final Color? bgColor;
   final Curve curve;
   final double? boxMaxHeight;
   final double? boxMaxWidth;
+  final double barsMinHeight;
+  final double barsMaxHeight;
   final EdgeInsetsGeometry? padding;
   final EdgeInsetsGeometry? margin;
   final BorderRadiusGeometry? borderRadius;
@@ -24,43 +26,65 @@ class WaveformComponent extends StatelessWidget {
     this.color,
     this.bgColor,
     this.curve = Curves.easeInOutQuart,
-    this.boxMaxHeight,
+    this.boxMaxHeight = 64.0,
     this.boxMaxWidth,
+    this.barsMinHeight = 3.0,
+    this.barsMaxHeight = 200.0,
     this.padding,
     this.margin,
     this.borderRadius = const BorderRadius.all(Radius.circular(12)),
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final downscaledList = WaveformController.inst.changeListSize(WaveformController.inst.curentWaveform, SettingsController.inst.waveformTotalBars.value);
+  State<WaveformComponent> createState() => _WaveformComponentState();
+}
 
-    return Container(
-      width: boxMaxWidth ?? Get.width,
-      height: boxMaxHeight ?? 64.0,
-      padding: padding,
-      margin: margin,
-      decoration: BoxDecoration(color: bgColor, borderRadius: borderRadius),
+class _WaveformComponentState extends State<WaveformComponent> {
+  late Widget _stockWidget;
+
+  @override
+  void initState() {
+    super.initState();
+    _fillWidget();
+  }
+
+  void _fillWidget() {
+    _stockWidget = Container(
+      key: widget.key,
+      width: widget.boxMaxWidth,
+      height: widget.boxMaxHeight,
+      padding: widget.padding,
+      margin: widget.margin,
+      decoration: BoxDecoration(color: widget.bgColor, borderRadius: widget.borderRadius),
       child: Obx(
-        () => Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            ...downscaledList.map(
-              (e) => AnimatedContainer(
-                duration: Duration(milliseconds: durationInMilliseconds),
-                height: (e * 100).clamp(3.0, 200.0),
-                width: Get.width / downscaledList.length * 0.45,
-                curve: curve,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0.multipliedRadius),
-                  color: color ?? CurrentColor.inst.color.value,
+        () {
+          final downscaledList = WaveformController.inst.changeListSize(WaveformController.inst.curentWaveform, SettingsController.inst.waveformTotalBars.value);
+          final barWidth = Get.width / downscaledList.length * 0.45;
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ...downscaledList.map(
+                (e) => AnimatedContainer(
+                  duration: Duration(milliseconds: widget.durationInMilliseconds),
+                  height: (e * 100).clamp(widget.barsMinHeight, widget.barsMaxHeight),
+                  width: barWidth,
+                  curve: widget.curve,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0.multipliedRadius),
+                    color: widget.color ?? CurrentColor.inst.color,
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          );
+        },
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _stockWidget;
   }
 }
