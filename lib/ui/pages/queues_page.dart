5c426a8ab764c3d818a4c5142d001653874d50fb
MSOB7YY <mohamedsobhy201566@gmail.com>
2023-11-28 23:44:19 +0200

    feat: dismissible queues #69

--- lib/ui/pages/queues_page.dart
+++ lib/ui/pages/queues_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scrollbar_modified/flutter_scrollbar_modified.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-
 import 'package:get/get.dart';
 
+import 'package:namida/controller/navigator_controller.dart';
 import 'package:namida/controller/queue_controller.dart';
 import 'package:namida/core/dimensions.dart';
 import 'package:namida/core/namida_converter_ext.dart';
+import 'package:namida/core/translations/language.dart';
 import 'package:namida/ui/widgets/custom_widgets.dart';
 import 'package:namida/ui/widgets/library/queue_tile.dart';
 
@@ -36,7 +37,26 @@ class QueuesPage extends StatelessWidget {
                       return AnimatingTile(
                         key: ValueKey(i),
                         position: i,
-                        child: QueueTile(queue: q),
+                        child: FadeDismissible(
+                          key: Key("${i}_${q.date}"),
+                          onDismissed: (onDismissed) {
+                            final oldQueue = q;
+                            QueueController.inst.removeQueue(oldQueue);
+                            snackyy(
+                              title: lang.UNDO_CHANGES,
+                              message: lang.UNDO_CHANGES_DELETED_QUEUE,
+                              displaySeconds: 3,
+                              button: TextButton(
+                                onPressed: () {
+                                  QueueController.inst.reAddQueue(oldQueue);
+                                  Get.closeAllSnackbars();
+                                },
+                                child: Text(lang.UNDO),
+                              ),
+                            );
+                          },
+                          child: QueueTile(queue: q),
+                        ),
                       );
                     },
                   );
