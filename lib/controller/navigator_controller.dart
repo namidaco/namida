namidaco/namida

commit a33dff060e3b41048f0038cfbaeed3ffa4426db5

perf: assigning instead of ..clear()..addAll() and color extraction delay in home page

lib/controller/navigator_controller.dart
@@ -315,9 +315,7 @@ class NamidaNavigator {
     bool nested = true,
     Transition transition = Transition.cupertino,
   }) async {
-    currentWidgetStack
-      ..clear()
-      ..add(page.toNamidaRoute());
+    currentWidgetStack.value = [page.toNamidaRoute()];
     _hideEverything();
 
     currentRoute?.updateColorScheme();

lib/ui/pages/home_page.dart
@@ -58,7 +58,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   final _randomTracks = <Track>[];
   final _recentListened = <TrackWithDate>[];
   final _topRecentListened = <MapEntry<Track, List<int>>>[];
-  final _sameTimeYearAgo = <MapEntry<Track, List<int>>>[];
+  var _sameTimeYearAgo = <MapEntry<Track, List<int>>>[];
 
   final _recentAlbums = <String>[];
   final _recentArtists = <String>[];
@@ -150,18 +150,14 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   }
 
   void _updateSameTimeNYearsAgo(DateTime timeNow, int year) {
-    _sameTimeYearAgo
-      ..clear()
-      ..addAll(
-        HistoryController.inst.getMostListensInTimeRange(
-          mptr: MostPlayedTimeRange.custom,
-          customDate: DateRange(
-            oldest: DateTime(year, timeNow.month, timeNow.day - 5),
-            newest: DateTime(year, timeNow.month, timeNow.day + 5),
-          ),
-          isStartOfDay: false,
-        ),
-      );
+    _sameTimeYearAgo = HistoryController.inst.getMostListensInTimeRange(
+      mptr: MostPlayedTimeRange.custom,
+      customDate: DateRange(
+        oldest: DateTime(year, timeNow.month, timeNow.day - 5),
+        newest: DateTime(year, timeNow.month, timeNow.day + 5),
+      ),
+      isStartOfDay: false,
+    );
     currentYearLostMemories = year;
   }
 
@@ -748,6 +744,15 @@ class _MixesCardState extends State<_MixesCard> {
   Color? _cardColor;
   Track? track;
 
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500)).then((value) {
+      _assignTrack();
+      _extractColor();
+    });
+  }
+
   void onMixTap(Widget thumbnailWidget) {
     NamidaNavigator.inst.navigateDialog(
       colorScheme: _cardColor,
@@ -825,8 +830,7 @@ class _MixesCardState extends State<_MixesCard> {
   void _extractColor() {
     if (track != null && _cardColor == null) {
       CurrentColor.inst.getTrackColors(track!, useIsolate: true).then((value) {
-        _cardColor = value.color;
-        if (mounted) setState(() {});
+        if (mounted) setState(() => _cardColor = value.color);
       });
     }
   }
@@ -858,8 +862,6 @@ class _MixesCardState extends State<_MixesCard> {
 
   @override
   Widget build(BuildContext context) {
-    _assignTrack();
-    _extractColor();
     final displayShimmer = track == null;
     Widget artworkWidget(bool titleInside) => NamidaHero(
           tag: 'mix_thumbnail_${widget.title}${widget.index}',
@@ -1053,15 +1055,19 @@ class _TrackCardState extends State<_TrackCard> {
   void _extractColor() {
     if (widget.track != null && _cardColor == null) {
       CurrentColor.inst.getTrackColors(widget.track!, useIsolate: true).then((value) {
-        _cardColor = value.color;
-        if (mounted) setState(() {});
+        if (mounted) setState(() => _cardColor = value.color);
       });
     }
   }
 
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500)).then((value) => _extractColor());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    _extractColor();
     final track = widget.track;
     final color = Color.alphaBlend((_cardColor ?? context.theme.scaffoldBackgroundColor).withAlpha(50), context.theme.cardColor);
     final dummyContainer = track == null;
@@ -1075,8 +1081,7 @@ class _TrackCardState extends State<_TrackCard> {
     }
     return NamidaInkWell(
       onTap: () {
-        _enabledTrack = (widget.title, widget.index);
-        if (mounted) setState(() {});
+        if (mounted) setState(() => _enabledTrack = (widget.title, widget.index));
 
         Player.inst.playOrPause(
           widget.index,

lib/youtube/controller/youtube_history_controller.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:history_manager/history_manager.dart';
@@ -39,13 +40,12 @@ class YoutubeHistoryController with HistoryManager<YoutubeID, String> {
   String mainItemToSubItem(YoutubeID item) => item.id;
 
   @override
-  Future<Map<int, List<YoutubeID>>> prepareAllHistoryFilesFunction(String directoryPath) async {
-    final map = await _readHistoryFilesCompute.thready(directoryPath);
-    return map;
+  Future<SplayTreeMap<int, List<YoutubeID>>> prepareAllHistoryFilesFunction(String directoryPath) async {
+    return await _readHistoryFilesCompute.thready(directoryPath);
   }
 
-  static Future<Map<int, List<YoutubeID>>> _readHistoryFilesCompute(String path) async {
-    final map = <int, List<YoutubeID>>{};
+  static Future<SplayTreeMap<int, List<YoutubeID>>> _readHistoryFilesCompute(String path) async {
+    final map = SplayTreeMap<int, List<YoutubeID>>();
     for (final f in Directory(path).listSyncSafe()) {
       if (f is File) {
         try {

lib/controller/audio_handler.dart
@@ -57,12 +57,10 @@ class NamidaAudioVideoHandler<Q extends Playable> extends BasicAudioHandler<Q> {
 
   Future<void> updateAudioCacheMap() async {
     final map = await _getAllAudiosInCache.thready(AppDirs.AUDIOS_CACHE);
-    audioCacheMap
-      ..clear()
-      ..addAll(map);
+    audioCacheMap = map;
   }
 
-  final audioCacheMap = <String, List<AudioCacheDetails>>{};
+  var audioCacheMap = <String, List<AudioCacheDetails>>{};
 
   Selectable get currentTrack => (currentItem is Selectable ? currentItem as Selectable : null) ?? kDummyTrack;
   YoutubeID? get currentVideo => currentItem is YoutubeID ? currentItem as YoutubeID : null;
@@ -591,9 +589,7 @@ class NamidaAudioVideoHandler<Q extends Playable> extends BasicAudioHandler<Q> {
 
         if (currentItem is YoutubeID && videoId != (currentItem as YoutubeID).id) return;
 
-        YoutubeController.inst.currentYTQualities
-          ..clear()
-          ..addAll(newStreams);
+        YoutubeController.inst.currentYTQualities.value = newStreams;
 
         if (sameStreamUrl != null) {
           await VideoController.vcontroller.setNetworkSource(
@@ -659,9 +655,7 @@ class NamidaAudioVideoHandler<Q extends Playable> extends BasicAudioHandler<Q> {
 
         if (currentItem is YoutubeID && videoId != (currentItem as YoutubeID).id) return;
 
-        YoutubeController.inst.currentYTAudioStreams
-          ..clear()
-          ..addAll(newStreams);
+        YoutubeController.inst.currentYTAudioStreams.value = newStreams;
 
         if (sameStreamUrl != null) {
           await setAudioLockCache();
@@ -810,12 +804,8 @@ class NamidaAudioVideoHandler<Q extends Playable> extends BasicAudioHandler<Q> {
         _isFetchingInfo.value = false;
         if (streams == null) return;
         if (item != currentVideo) return; // race avoidance when playing multiple videos
-        YoutubeController.inst.currentYTQualities
-          ..clear()
-          ..addAll(streams.videoOnlyStreams ?? []);
-        YoutubeController.inst.currentYTAudioStreams
-          ..clear()
-          ..addAll(streams.audioOnlyStreams ?? []);
+        YoutubeController.inst.currentYTQualities.value = streams.videoOnlyStreams ?? [];
+        YoutubeController.inst.currentYTAudioStreams.value = streams.audioOnlyStreams ?? [];
         currentVideoInfo.value = streams.videoInfo;
         final vos = streams.videoOnlyStreams;
         final allVideoStream = isAudioOnlyPlayback || vos == null || vos.isEmpty ? null : YoutubeController.inst.getPreferredStreamQuality(vos, preferIncludeWebm: false);
@@ -960,9 +950,7 @@ class NamidaAudioVideoHandler<Q extends Playable> extends BasicAudioHandler<Q> {
       (e) => e.frameratePrecise,
     );
 
-    YoutubeController.inst.currentCachedQualities
-      ..clear()
-      ..addAll(allCachedVideos);
+    YoutubeController.inst.currentCachedQualities.value = allCachedVideos;
 
     final cachedVideo = allCachedVideos.firstOrNull;
     final mediaItem = item.toMediaItem(currentVideoInfo.value, currentVideoThumbnail.value, index, currentQueue.length);

lib/controller/folders_controller.dart
@@ -55,9 +55,7 @@ class Folders {
       return;
     }
 
-    currentFolderslist
-      ..clear()
-      ..addAll(dirInside);
+    currentFolderslist.value = dirInside;
 
     currentFolder.value = folder;
     if (trackToScrollTo != null) {

lib/controller/history_controller.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:history_manager/history_manager.dart';
@@ -107,13 +108,12 @@ class HistoryController with HistoryManager<TrackWithDate, Track> {
   Track mainItemToSubItem(TrackWithDate item) => item.track;
 
   @override
-  Future<Map<int, List<TrackWithDate>>> prepareAllHistoryFilesFunction(String directoryPath) async {
-    final map = await _readHistoryFilesCompute.thready(directoryPath);
-    return map;
+  Future<SplayTreeMap<int, List<TrackWithDate>>> prepareAllHistoryFilesFunction(String directoryPath) async {
+    return await _readHistoryFilesCompute.thready(directoryPath);
   }
 
-  static Future<Map<int, List<TrackWithDate>>> _readHistoryFilesCompute(String path) async {
-    final map = <int, List<TrackWithDate>>{};
+  static Future<SplayTreeMap<int, List<TrackWithDate>>> _readHistoryFilesCompute(String path) async {
+    final map = SplayTreeMap<int, List<TrackWithDate>>((date1, date2) => date2.compareTo(date1));
     for (final f in Directory(path).listSyncSafe()) {
       if (f is File) {
         try {

lib/controller/indexer_controller.dart
@@ -1032,9 +1032,7 @@ class Indexer {
   Future<void> readTrackData() async {
     // reading stats file containing track rating etc.
     final statsResult = await _readTracksStatsCompute.thready(AppPaths.TRACKS_STATS);
-    trackStatsMap
-      ..clear()
-      ..addAll(statsResult);
+    trackStatsMap.value = statsResult;
 
     tracksInfoList.clear(); // clearing for cases which refreshing library is required (like after changing separators)
 
@@ -1045,9 +1043,7 @@ class Indexer {
       genresConfig: GenresSplitConfig.settings(),
     );
     final tracksResult = await _readTracksFileCompute.thready(splitconfig);
-    allTracksMappedByPath
-      ..clear()
-      ..addAll(tracksResult);
+    allTracksMappedByPath.value = tracksResult;
     tracksInfoList.addAll(tracksResult.keys);
 
     printy("All Tracks Length From File: ${tracksInfoList.length}");
@@ -1194,9 +1190,8 @@ class Indexer {
 
     tracksExcludedByNoMedia.value += excludedByNoMedia.length;
 
-    allAudioFiles
-      ..clear()
-      ..addAll(allPaths);
+    // ignore: invalid_use_of_protected_member
+    allAudioFiles.value = allPaths;
 
     printy("Paths Found: ${allPaths.length}");
     return allPaths;

lib/controller/json_to_history_parser.dart
@@ -331,9 +331,7 @@ class JsonToHistoryParser {
     _notificationTimer?.cancel();
     NotificationService.inst.doneImportingHistoryNotification(parsedHistoryJson.value, addedHistoryJsonToPlaylist.value);
 
-    _latestMissingMap
-      ..clear()
-      ..addAll(allMissingEntries);
+    _latestMissingMap.value = allMissingEntries;
     _latestMissingMapAddedStatus.clear();
     showMissingEntriesDialog();
   }

commit 19f6b11d42fbfa944c93ad676f7c6cb638530dc0

chore: minor fixed/tweaks + isInYTCommentsSubpage check when popping in yt miniplayer + longer doubleTapSeekReset (900ms) + image delay bug introduced in a33dff0 + unsorted yt history bug introduced in a33dff0

lib/controller/navigator_controller.dart
@@ -336,7 +336,7 @@ class NamidaNavigator {
     }
 
     final ytmpcpks = ytMiniplayerCommentsPageKey?.currentState;
-    if (ytmpcpks != null) {
+    if (isInYTCommentsSubpage && ytmpcpks != null) {
       ytmpcpks.pop();
       isInYTCommentsSubpage = false;
       return;

lib/ui/pages/home_page.dart
@@ -747,10 +747,8 @@ class _MixesCardState extends State<_MixesCard> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(milliseconds: 500)).then((value) {
-      _assignTrack();
-      _extractColor();
-    });
+    _assignTrack();
+    Future.delayed(const Duration(milliseconds: 500)).then((value) => _extractColor());
   }
 
   void onMixTap(Widget thumbnailWidget) {

lib/ui/widgets/video_widget.dart
@@ -81,7 +81,7 @@ class NamidaVideoControlsState extends State<NamidaVideoControls> with TickerPro
   final volumeHideDuration = const Duration(milliseconds: 500);
   final brightnessHideDuration = const Duration(milliseconds: 500);
   final transitionDuration = const Duration(milliseconds: 300);
-  final doubleTapSeekReset = const Duration(milliseconds: 600);
+  final doubleTapSeekReset = const Duration(milliseconds: 900);
 
   Timer? _hideTimer;
   void _resetTimer({bool hideControls = false}) {

lib/youtube/controller/youtube_history_controller.dart
@@ -45,7 +45,7 @@ class YoutubeHistoryController with HistoryManager<YoutubeID, String> {
   }
 
   static Future<SplayTreeMap<int, List<YoutubeID>>> _readHistoryFilesCompute(String path) async {
-    final map = SplayTreeMap<int, List<YoutubeID>>();
+    final map = SplayTreeMap<int, List<YoutubeID>>((date1, date2) => date2.compareTo(date1));
     for (final f in Directory(path).listSyncSafe()) {
       if (f is File) {
         try {