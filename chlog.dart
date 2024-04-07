8cac89e9312875fd3912b19ed41337ca4b68b68c
MSOB7YY <mohamedsobhy201566@gmail.com>
2024-04-07 17:11:06 +0200

    build: bump version (v2.0.6)

--- chlog.dart
+++ chlog.dart
@@ -66,7 +66,9 @@ void main(List<String> argumentsPre) async {
   for (final line in outputLines) {
     final parts = line.split(':');
     final firstPart = parts.first.toLowerCase().trim().split(' ');
-    final hash = firstPart.first;
+    final hashFull = firstPart.first;
+    if (hashFull == '') continue;
+    final hash = hashFull.substring(0, 7);
     final key = firstPart.last;
     final val = "$hash: ${parts.skip(1).join('').trim()}";
     if (map[key] == null) {
@@ -75,9 +77,41 @@ void main(List<String> argumentsPre) async {
       map[key]!.add(val);
     }
   }
+  const prettyMapNeededKeys = ['feat', 'chore', 'fix', 'perf', 'core'];
+  final regexCloseIssue = RegExp(r'.+?(?= closes #)');
+  final regexRefIssue = RegExp(r'.+?(?= ref #)');
+  final prettyMap = <String, List<String>>{};
+  for (final line in outputLines) {
+    final parts = line.split(':');
+    final firstPart = parts.first.toLowerCase().trim().split(' ');
+    final key = firstPart.last;
+    if (!prettyMapNeededKeys.contains(key)) continue;
+    final val = parts.skip(1).join('').trim();
+    final valTrimmed = regexCloseIssue.firstMatch(val)?[0] ?? regexRefIssue.firstMatch(val)?[0] ?? val;
+    if (prettyMap[key] == null) {
+      prettyMap[key] = [valTrimmed];
+    } else {
+      prettyMap[key]!.add(valTrimmed);
+    }
+  }
+
+  String versionName = 'unknown';
+  for (final line in File('pubspec.yaml').readAsLinesSync()) {
+    if (line.startsWith('version: ')) {
+      versionName = "v${line.split('version: ').last.split('+').first}";
+      break;
+    }
+  }
   final stringy = map.entries.map((e) => '- ${e.key}:\n${e.value.reversed.map((e) => '   - $e').join('\n')}');
   final dateText = DateFormat('dd/MM/yyyy').format(stopDatetime ?? DateTime.now());
   const title = '# Namida Changelog';
-  final finalString = "$title\n\n## $dateText\n${stringy.join('\n')}\n${changelog.readAsStringSync().replaceFirst(title, '')}";
+  final finalString = "$title\n\n## $dateText\n# $versionName\n${stringy.join('\n')}\n${changelog.readAsStringSync().replaceFirst(title, '')}";
   changelog.writeAsStringSync(finalString);
+
+  final stringyFeat = (prettyMap['feat'] ?? []).reversed.map((e) => '   • $e').join('\n');
+  final stringyBugImpr = prettyMap.entries.map((e) => '- ${e.key}:\n${e.value.reversed.map((e) => '   • $e').join('\n')}\n');
+
+  File('CHANGELOG_pretty.txt')
+    ..createSync()
+    ..writeAsStringSync("🎉 New Features:\n$stringyFeat\n\n🛠️ Bug fixes & Improvements:\n${stringyBugImpr.join('\n')}");
 }
