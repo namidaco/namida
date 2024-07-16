adea6dbcbb044a88f7965a36e3af5fc91a08505a
MSOB7YY <mohamedsobhy201566@gmail.com>
2024-07-17 00:48:09 +0300

    chore(yt): show account header in yt playlists page

--- lib/youtube/pages/youtube_user_playlists_page.dart
+++ lib/youtube/pages/youtube_user_playlists_page.dart
@@ -3,12 +3,18 @@ import 'package:youtipie/class/result_wrapper/playlist_user_result.dart';
 import 'package:youtipie/class/youtipie_feed/playlist_info_item_user.dart';
 import 'package:youtipie/youtipie.dart';
 
+import 'package:namida/class/route.dart';
 import 'package:namida/core/dimensions.dart';
+import 'package:namida/core/icon_fonts/broken_icons.dart';
 import 'package:namida/core/translations/language.dart';
+import 'package:namida/core/utils.dart';
+import 'package:namida/youtube/controller/youtube_account_controller.dart';
 import 'package:namida/youtube/controller/youtube_info_controller.dart';
+import 'package:namida/youtube/pages/user/youtube_account_manage_page.dart';
 import 'package:namida/youtube/pages/youtube_main_page_fetcher_acc_base.dart';
 import 'package:namida/youtube/pages/youtube_user_history_page.dart';
 import 'package:namida/youtube/widgets/yt_playlist_card.dart';
+import 'package:namida/youtube/widgets/yt_thumbnail.dart';
 import 'package:namida/youtube/widgets/yt_video_card.dart';
 
 class YoutubeUserPlaylistsPage extends StatelessWidget {
@@ -25,7 +31,13 @@ class YoutubeUserPlaylistsPage extends StatelessWidget {
     return YoutubeMainPageFetcherAccBase<YoutiPieUserPlaylistsResult, PlaylistInfoItemUser>(
         transparentShimmer: true,
         topPadding: 12.0,
-        pageHeader: horizontalHistory,
+        pageHeader: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const _AccountHeader(),
+            horizontalHistory,
+          ],
+        ),
         onPullToRefresh: () => (horizontalHistoryKey.currentState as dynamic)?.forceFetchFeed() as Future<void>,
         title: lang.PLAYLISTS,
         cacheReader: YoutiPie.cacheBuilder.forUserPlaylists(),
@@ -49,5 +61,61 @@ class YoutubeUserPlaylistsPage extends StatelessWidget {
             playOnTap: false,
           );
         });
+  }
+}
+
+class _AccountHeader extends StatelessWidget {
+  const _AccountHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return ObxO(
+      rx: YoutubeAccountController.current.activeAccountChannel,
+      builder: (acc) => acc == null
+          ? const SizedBox()
+          : Padding(
+              padding: const EdgeInsets.only(top: 24.0, bottom: 12.0),
+              child: Row(
+                children: [
+                  const SizedBox(width: 24.0),
+                  YoutubeThumbnail(
+                    type: ThumbnailType.channel,
+                    key: Key(acc.id),
+                    width: 54.0,
+                    forceSquared: false,
+                    isImportantInCache: true,
+                    customUrl: acc.thumbnails.pick()?.url,
+                    isCircle: true,
+                  ),
+                  const SizedBox(width: 12.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          acc.title,
+                          style: context.textTheme.displayMedium,
+                        ),
+                        const SizedBox(height: 2.0),
+                        Text(
+                          acc.handler,
+                          style: context.textTheme.displaySmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 12.0),
+                  IconButton.filledTonal(
+                    onPressed: const YoutubeAccountManagePage().navigate,
+                    icon: const Icon(
+                      Broken.edit,
+                    ),
+                  ),
+                  const SizedBox(width: 18.0),
+                ],
+              ),
+            ),
+    );
   }
 }
