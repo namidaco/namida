namidaco/namida

commit 797351a51b16bb33c06d047570b73536038b076c

perf: more efficient and reliable extracting paths key

pubspec.yaml
@@ -1,7 +1,7 @@
 name: namida
 description: A Beautiful and Feature-rich Music Player, With YouTube & Video Support Built in Flutter
 publish_to: "none"
-version: 5.1.06-beta+250411228
+version: 5.1.07-beta+250411232
 
 environment:
   sdk: ">=3.6.0 <4.0.0"

lib/controller/indexer_controller.dart
@@ -875,8 +875,10 @@ class Indexer<T extends Track> {
 
     tracksMissing.loop((e) => onProgress(false));
 
+    final keyWrapper = ExtractingPathKey.create();
     final stream = await NamidaTaggerController.inst.extractMetadataAsStream(
       paths: tracksRealPaths,
+      keyWrapper: keyWrapper,
       overrideArtwork: updateArtwork,
     );
     final splitConfigs = _createSplitConfig();
@@ -983,7 +985,11 @@ class Indexer<T extends Track> {
             splittersConfigs: splitConfig,
           );
 
-      final stream = await NamidaTaggerController.inst.extractMetadataAsStream(paths: tracksToExtract);
+      final keyWrapper = ExtractingPathKey.create();
+      final stream = await NamidaTaggerController.inst.extractMetadataAsStream(
+        paths: tracksToExtract,
+        keyWrapper: keyWrapper,
+      );
       await for (final item in stream) {
         final p = item.tags.path;
         final obj = Track.orVideo(p);
@@ -1087,6 +1093,7 @@ class Indexer<T extends Track> {
       final listParts = (Platform.numberOfProcessors ~/ 2.5).withMinimum(1);
       final audioFilesParts = finalAudios.split(listParts);
       final audioFilesCompleters = List.generate(audioFilesParts.length, (_) => Completer<void>());
+      final keyWrapper = ExtractingPathKey.create();
 
       Future<void> extractAll(List<String> chunkList) async {
         if (chunkList.isEmpty) return;
@@ -1112,6 +1119,7 @@ class Indexer<T extends Track> {
 
         final stream = await NamidaTaggerController.inst.extractMetadataAsStream(
           paths: chunkList,
+          keyWrapper: keyWrapper,
           overrideArtwork: false,
         );
 

lib/controller/platform/tags_extractor/tags_extractor_android.dart
@@ -123,12 +123,13 @@ class _TagsExtractorAndroid extends TagsExtractor {
   @override
   Future<Stream<FAudioModel>> extractMetadataAsStream({
     required List<String> paths,
+    required ExtractingPathKey keyWrapper,
     bool extractArtwork = true,
     required String? audioArtworkDirectory,
     required String? videoArtworkDirectory,
     bool overrideArtwork = false,
   }) async {
-    final streamKey = DateTime.now().microsecondsSinceEpoch;
+    final streamKey = keyWrapper.next();
     StreamSubscription<dynamic>? streamSub;
     StreamSubscription<dynamic>? streamSubIndices;
     final identifiersSet = TagsExtractor.getAlbumIdentifiersSet();

lib/controller/platform/tags_extractor/tags_extractor_base.dart
@@ -31,6 +31,7 @@ abstract class TagsExtractor {
 
   FutureOr<Stream<FAudioModel>> extractMetadataAsStream({
     required List<String> paths,
+    required ExtractingPathKey keyWrapper,
     bool extractArtwork = true,
     required String? audioArtworkDirectory,
     required String? videoArtworkDirectory,
@@ -107,4 +108,11 @@ abstract class TagsExtractor {
       identifiers: identifiers,
     );
   }
+}
+
+class ExtractingPathKey {
+  int _initial = 0;
+  ExtractingPathKey.create();
+
+  int next() => _initial++;
 }

lib/controller/platform/tags_extractor/tags_extractor_windows.dart
@@ -72,12 +72,13 @@ class _TagsExtractorWindows extends TagsExtractor {
   @override
   Stream<FAudioModel> extractMetadataAsStream({
     required List<String> paths,
+    required ExtractingPathKey keyWrapper,
     bool extractArtwork = true,
     required String? audioArtworkDirectory,
     required String? videoArtworkDirectory,
     bool overrideArtwork = false,
   }) async* {
-    final key = DateTime.now().microsecondsSinceEpoch;
+    final key = keyWrapper.next();
     for (int i = 0; i < paths.length; i++) {
       var path = paths[i];
       currentPathsBeingExtracted[key] = path;
@@ -92,6 +93,7 @@ class _TagsExtractorWindows extends TagsExtractor {
       );
       yield info;
     }
+    currentPathsBeingExtracted.remove(key);
   }
 
   @override

lib/controller/tagger_controller.dart
@@ -28,12 +28,14 @@ class NamidaTaggerController {
 
   Future<Stream<FAudioModel>> extractMetadataAsStream({
     required List<String> paths,
+    required ExtractingPathKey keyWrapper,
     bool extractArtwork = true,
     bool saveArtworkToCache = true,
     bool overrideArtwork = false,
   }) async {
     return _extractor.extractMetadataAsStream(
       paths: paths,
+      keyWrapper: keyWrapper,
       extractArtwork: extractArtwork,
       audioArtworkDirectory: saveArtworkToCache ? AppDirs.ARTWORKS : null,
       videoArtworkDirectory: saveArtworkToCache ? AppDirs.THUMBNAILS : null,

commit 44d7c16dbf724d6f541e37b629b117dc7999fccb

fix: type error in kotlin side (Long -> Number) - caused by: 797351a

android/app/src/main/kotlin/com/msob7y/namida/FAudioTagger.kt
@@ -39,8 +39,8 @@ public class FAudioTagger : FlutterPlugin, MethodCallHandler {
 
   lateinit var channel: MethodChannel
   lateinit var binaryMessenger: BinaryMessenger
-  val eventChannels = HashMap<Long, BetterEventChannel>()
-  val streamCompleters = HashMap<Long, CompletableFuture<Number>>()
+  val eventChannels = HashMap<Number, BetterEventChannel>()
+  val streamCompleters = HashMap<Number, CompletableFuture<Number>>()
   lateinit var context: Context
 
   companion object {
@@ -116,7 +116,7 @@ public class FAudioTagger : FlutterPlugin, MethodCallHandler {
         }
       }
       "streamReady" -> {
-        val streamKey = call.argument<Long?>("streamKey") ?: 0
+        val streamKey = call.argument<Number?>("streamKey") ?: 0
         val count = call.argument<Number?>("count") ?: 0
         val completer = streamCompleters.get(streamKey)
         if (completer != null) {
@@ -137,7 +137,7 @@ public class FAudioTagger : FlutterPlugin, MethodCallHandler {
           val videoExtensions = videoExtensionsList?.toSet()
           val extractArtwork = call.argument<Boolean?>("extractArtwork") ?: true
           val overrideArtwork = call.argument<Boolean?>("overrideArtwork") ?: false
-          val streamKey = call.argument<Long?>("streamKey") ?: 0
+          val streamKey = call.argument<Number?>("streamKey") ?: 0
           eventChannels.set(
               streamKey,
               BetterEventChannel(binaryMessenger, "faudiotagger/stream/" + streamKey)

pubspec.yaml
@@ -1,7 +1,7 @@
 name: namida
 description: A Beautiful and Feature-rich Music Player, With YouTube & Video Support Built in Flutter
 publish_to: "none"
-version: 5.1.15-beta+250416157
+version: 5.1.16-beta+250416159
 
 environment:
   sdk: ">=3.6.0 <4.0.0"