5eeaec994b271845df129663b018633d649c7553
MSOB7YY <mohamedsobhy201566@gmail.com>
2025-10-08 21:07:51 +0200

    chore(windows): ensure window is within screen bounds (ex. after disconnecting a second screen)

--- lib/controller/platform/window_manager/window_manager.dart
+++ lib/controller/platform/window_manager/window_manager.dart
@@ -2,10 +2,13 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+// ignore: depend_on_referenced_packages
+import 'package:screen_retriever/screen_retriever.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'package:namida/controller/platform/base.dart';
 import 'package:namida/controller/settings_controller.dart';
+import 'package:namida/core/utils.dart';
 import 'package:namida/ui/widgets/artwork.dart';
 
 part 'window_manager_base.dart';
--- lib/controller/platform/window_manager/window_manager_desktop.dart
+++ lib/controller/platform/window_manager/window_manager_desktop.dart
@@ -18,10 +18,80 @@ class _WindowManagerDesktop extends NamidaWindowManager {
     windowManager.addListener(_NamidaWindowListener());
     await windowManager.waitUntilReadyToShow(windowOptions, () async {
       final bounds = settings.windowBounds;
-      if (bounds != null) await windowManager.setBounds(settings.windowBounds);
+      if (bounds != null) {
+        // -- making sure window is in bounds with the current screen/s max size
+        // -- for example: after disconnecting a second screen
+        final shiftedBounds = await _ensureBoundsWithinScreenSizeShift(bounds);
+        await windowManager.setBounds(shiftedBounds);
+      }
       await windowManager.show();
       await windowManager.focus();
     });
+
+    screenRetriever.addListener(_CustomScreenListener(
+      listener: (_) async {
+        // display-removed or display-added
+        final bounds = await windowManager.getBounds();
+        final shiftedBounds = await _ensureBoundsWithinScreenSizeShift(bounds);
+        if (bounds != shiftedBounds) {
+          await windowManager.setBounds(shiftedBounds);
+        }
+      },
+    ));
+  }
+
+  Future<Rect> _ensureBoundsWithinScreenSizeShift(Rect bounds) async {
+    // virtual desktop area (union of all monitors)
+    double minX = double.infinity;
+    double minY = double.infinity;
+    double maxX = double.negativeInfinity;
+    double maxY = double.negativeInfinity;
+
+    final displays = await screenRetriever.getAllDisplays();
+    for (final d in displays) {
+      final rect = Rect.fromLTWH(
+        d.visiblePosition?.dx ?? 0,
+        d.visiblePosition?.dy ?? 0,
+        d.size.width,
+        d.size.height,
+      );
+      minX = minX < rect.left ? minX : rect.left;
+      minY = minY < rect.top ? minY : rect.top;
+      maxX = maxX > rect.right ? maxX : rect.right;
+      maxY = maxY > rect.bottom ? maxY : rect.bottom;
+    }
+
+    if (minX == double.infinity) {
+      // -- fallback
+      minX = 0;
+      minY = 0;
+      maxX = namida.width;
+      maxY = namida.height;
+    }
+
+    final desktopRect = Rect.fromLTRB(minX, minY, maxX, maxY);
+
+    final windowWidth = bounds.width;
+    final windowHeight = bounds.height;
+
+    // -- clamp position while keeping size
+    final newLeft = bounds.left.clamp(
+      desktopRect.left,
+      desktopRect.right - windowWidth,
+    );
+    final newTop = bounds.top.clamp(
+      desktopRect.top,
+      desktopRect.bottom - windowHeight,
+    );
+
+    final shiftedBounds = Rect.fromLTWH(
+      newLeft,
+      newTop,
+      windowWidth,
+      windowHeight,
+    );
+
+    return shiftedBounds;
   }
 }
 
@@ -45,4 +115,13 @@ class _NamidaWindowListener with WindowListener {
   void onWindowMoved() async {
     await _saveBounds();
   }
+}
+
+class _CustomScreenListener extends ScreenListener {
+  final Function(String eventName) listener;
+
+  _CustomScreenListener({required this.listener});
+
+  @override
+  void onScreenEvent(String eventName) => listener(eventName);
 }
