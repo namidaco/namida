namidaco/namida

commit 20e56941f748537acc763cc95d959024c8d61278

chore: improve year tag extraction ref: #767

lib/class/track.dart
@@ -325,17 +325,43 @@ class TrackExtended {
   });
 
   static String _padInt(int val) => val.toString().padLeft(2, '0');
+  static String _padYear(int val) {
+    if (val < 100) return '20$val';
+    return val.toString();
+  }
 
-  static final _yearFormatRegex = RegExp(r'[\s]');
+  static final _extractNumbersRegex = RegExp(r'\d+');
   static int? enforceYearFormat(String? fromYearString) {
-    final intVal = fromYearString.getIntValue();
-    if (intVal != null) return intVal;
-    if (fromYearString != null) {
-      try {
-        final yearDate = DateTime.parse(fromYearString.replaceAll(_yearFormatRegex, '-'));
-        return int.parse("${yearDate.year}${_padInt(yearDate.month)}${_padInt(yearDate.day)}");
-      } catch (_) {}
+    if (fromYearString == null) return null;
+    if (fromYearString.length == 4) {
+      final intVal = fromYearString.getIntValue();
+      if (intVal != null && intVal > 1000) return intVal;
     }
+    final dateParts = <int>[];
+    final matches = _extractNumbersRegex.allMatches(fromYearString);
+    for (final m in matches) {
+      final text = m[0];
+      if (text != null && text.isNotEmpty) {
+        final value = int.tryParse(text);
+        if (value != null) {
+          dateParts.add(value);
+        }
+      }
+    }
+    if (dateParts.isEmpty) return null;
+
+    try {
+      final buffer = StringBuffer();
+      buffer.write(_padYear(dateParts[0]));
+      if (dateParts.length > 1) buffer.write(_padInt(dateParts[1]));
+      if (dateParts.length > 2) {
+        buffer.write(_padInt(dateParts[2]));
+      } else {
+        buffer.write('01');
+      }
+      return int.parse(buffer.toString());
+    } catch (_) {}
+
     return null;
   }
 

commit 76d6cffd6969cb6ba7b0dd5dbb05c1b43e2b78fd

fix: year tag extraction caused by 20e5694 ref: #767

lib/class/track.dart
@@ -352,12 +352,17 @@ class TrackExtended {
 
     try {
       final buffer = StringBuffer();
-      buffer.write(_padYear(dateParts[0]));
-      if (dateParts.length > 1) buffer.write(_padInt(dateParts[1]));
-      if (dateParts.length > 2) {
-        buffer.write(_padInt(dateParts[2]));
-      } else {
-        buffer.write('01');
+      final yearText = _padYear(dateParts[0]); // can be 2/4 digits (separated) or 6 yyyyMM or 8 yyyyMMdd
+      buffer.write(yearText);
+      if (dateParts.length > 1) {
+        buffer.write(_padInt(dateParts[1]));
+        if (dateParts.length > 2) {
+          buffer.write(_padInt(dateParts[2]));
+        } else {
+          buffer.write('01'); // only add day 01 if month was there
+        }
+      } else if (yearText.length == 6) {
+        buffer.write('01'); // also add day 01 if year was 6 yyyyMM
       }
       return int.parse(buffer.toString());
     } catch (_) {}